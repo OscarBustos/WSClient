0f3e8370126c728b9523ccc4000e4582d6f23842
Oscar <Oscar@10.21.118.37>
2016-03-15 19:27:43 -0500

    Primer commit

---
+++ src/loadService.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Java\jdk1.7.0_45\bin\wsimport.exe" -keep -s src "http://localhost:8084/WSSumar/Sumar?wsdl"
+pause
---
+++ src/oscar/bustos/ws/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.bustos.oscar/")
+package oscar.bustos.ws;
